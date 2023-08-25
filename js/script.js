99fe3d7ae81167ceaddb9e437f9204bf5e33a7c5
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-08-25 11:57:37 +0200

    add if else for bookmark

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 const card = document.querySelector('[data-js="question-card"]');
 const bookmarkIcon = document.querySelector('[data-js="question-card__icon"]');
 
+let bookmarkClicked = false;
+
 card.addEventListener("click", () => {
   console.log("card is clicked");
 });
 
 function toggleFirstBookmark() {
-  let bookmarkImage = "./assets/bookmark_filled.png";
-  //   bookmarkIcon.src = svgBookmark;
-  bookmarkIcon.setAttribute("src", bookmarkImage);
+  let bookmarkImage = "./assets/bookmark.png";
+  let bookmarkImageFilled = "./assets/bookmark_filled.png";
+
+  if (bookmarkClicked) {
+    bookmarkIcon.setAttribute("src", bookmarkImage);
+    bookmarkClicked = false;
+  } else {
+    bookmarkIcon.setAttribute("src", bookmarkImageFilled);
+    bookmarkClicked = true;
+  }
 }
 
 bookmarkIcon.addEventListener("click", (event) => {
